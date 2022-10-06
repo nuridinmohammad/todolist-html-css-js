68e7ef81fb2b38fe3d77f28119c0bc6118ba07f5
Mohammad Nuridin <developer.mohammadnuridin@gmail.com>
2022-10-06 23:29:22 +0700

    update main.js

--- v1/main.js
+++ v1/main.js
@@ -1,14 +1,33 @@
 const taskInput = document.querySelector(".task-input input");
 const taskBox = document.querySelector(".task-box");
+const filters = document.querySelectorAll(".filters span");
+const clearAll = document.querySelector(".clear-btn");
 
 let todos = JSON.parse(localStorage.getItem("todo-list"));
+let editId;
+let isEditTask = false;
 
-const showTodo = () => {
+clearAll.addEventListener("click", () => {
+  todos.splice(0, todos.length);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
+});
+
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showTodo(btn.id);
+  });
+});
+
+const showTodo = (filtersId) => {
   let li = "";
   if (todos) {
     todos.forEach((todo, id) => {
       let isCompleted = todo.status == "completed" ? "checked" : "";
-      li += `<li class="task">
+      if (filtersId == todo.status || filtersId == "all") {
+        li += `<li class="task">
           <label for="${id}">
             <input type="checkbox" onclick="updateStatus(this)" name="" id="${id}" ${isCompleted}/>
             <p class="${isCompleted}">${todo.name}</p>
@@ -16,17 +35,24 @@ const showTodo = () => {
           <div class="settings">
             <i class="uil uil-ellipsis-h" onclick="showMenu(this)"></i>
             <ul class="task-menu">
-              <li><i class="uil uil-pen"></i>Edit</li>
-              <li><i class="uil uil-trash"></i>Delete</li>
+              <li onclick="editTask(${id}, '${todo.name}')"><i class="uil uil-pen"></i>Edit</li>
+              <li onclick="deleteTask(${id})"><i class="uil uil-trash"></i>Delete</li>
             </ul>
           </div>
         </li>`;
+      }
     });
   }
-  taskBox.innerHTML = li;
+  taskBox.innerHTML = li || `U don't have any task.. `;
 };
 
-showTodo();
+showTodo("all");
+
+const editTask = (id, name) => {
+  editId = id;
+  isEditTask = true;
+  taskInput.value = name;
+};
 
 const showMenu = (selectedTask) => {
   let taskMenu = selectedTask.parentElement.lastElementChild;
@@ -36,6 +62,12 @@ const showMenu = (selectedTask) => {
       taskMenu.classList.remove("show");
     }
   });
+};
+
+const deleteTask = (id) => {
+  todos.splice(id, 1);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
 };
 
 const updateStatus = (selectedTask) => {
@@ -54,15 +86,19 @@ taskInput.addEventListener("keyup", (e) => {
   let task = taskInput.value.trim();
 
   if (e.key == "Enter" && task) {
-    if (!todos) {
-      todos = [];
+    if (!isEditTask) {
+      if (!todos) {
+        todos = [];
+      }
+      let todo = { name: task, status: "pending" };
+      todos.push(todo);
+    } else {
+      isEditTask = false;
+      todos[editId].name = task;
     }
 
-    let todo = { name: task, status: "pending" };
-    todos.push(todo);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
     taskInput.value = "";
-    alert("Successfully added!");
-    showTodo();
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo("all");
   }
 });

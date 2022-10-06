5d07b1cfca9dc2273ae076813ca9f74c45c2e72e
Mohammad Nuridin <developer.mohammadnuridin@gmail.com>
2022-10-06 12:35:28 +0700

    update main.js

--- v1/main.js
+++ v1/main.js
@@ -7,13 +7,14 @@ const showTodo = () => {
   let li = "";
   if (todos) {
     todos.forEach((todo, id) => {
+      let isCompleted = todo.status == "completed" ? "checked" : "";
       li += `<li class="task">
           <label for="${id}">
-            <input type="checkbox" name="" id="${id}" />
-            <p>${todo.name}</p>
+            <input type="checkbox" onclick="updateStatus(this)" name="" id="${id}" ${isCompleted}/>
+            <p class="${isCompleted}">${todo.name}</p>
           </label>
           <div class="settings">
-            <i class="uil uil-ellipsis-h"></i>
+            <i class="uil uil-ellipsis-h" onclick="showMenu(this)"></i>
             <ul class="task-menu">
               <li><i class="uil uil-pen"></i>Edit</li>
               <li><i class="uil uil-trash"></i>Delete</li>
@@ -26,6 +27,29 @@ const showTodo = () => {
 };
 
 showTodo();
+
+const showMenu = (selectedTask) => {
+  let taskMenu = selectedTask.parentElement.lastElementChild;
+  taskMenu.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != selectedTask) {
+      taskMenu.classList.remove("show");
+    }
+  });
+};
+
+const updateStatus = (selectedTask) => {
+  let taskName = selectedTask.parentElement.lastElementChild;
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    todos[selectedTask.id].status = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].status = "pending";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+};
+
 taskInput.addEventListener("keyup", (e) => {
   let task = taskInput.value.trim();
 
